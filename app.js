3936f3b3a3fd12af1dac8954c5217035d9d72025
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-09-26 12:35:36 -0300

    handling errors

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 
-const userRoutes = require('./routes/users-routes');
-
 const app = express();
 
-app.use('/api/places', placesRoutes);
+app.use('/api/places', placesRoutes); // => /api/places...
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
 
 app.listen(5000);
